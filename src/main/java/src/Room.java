34dac33fd1b7833b9d0f876e0e763629a8933d5b
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2024-12-06 13:41:10 -0800

    refactoring Room file logic to introduce multiple choice options and hint functionality

--- src/main/java/src/Room.java
+++ src/main/java/src/Room.java
@@ -1,18 +1,29 @@
 package src;
 
 public class Room {
-    String name;       // The name of the room
-    String riddle;     // The riddle presented in this room
-    String answer;     // The answer to the riddle
-    boolean solved;    // Whether the riddle has been solved
-    Room nextRoom;     // Reference to the next room (linked list)
+    public String name;       // Name of the room
+    public String riddle;     // The riddle presented in the room
+    public String[] options;  // Multiple-choice options
+    public String answer;     // Correct answer
+    public String hint;       // Hint for the riddle
+    public boolean solved;    // Whether the riddle has been solved
+    public Room nextRoom;     // Reference to the next room (linked list)
 
     // Constructor
-    public Room(String name, String riddle, String answer) {
+    public Room(String name, String riddle, String[] options, String answer, String hint) {
         this.name = name;
         this.riddle = riddle;
+        this.options = options;
         this.answer = answer;
+        this.hint = hint;
         this.solved = false;  // Initially unsolved
-        this.nextRoom = null; // Initially, no next room
+        this.nextRoom = null; // No next room by default
+    }
+
+    // Display the multiple-choice options
+    public void displayOptions() {
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i + 1) + ". " + options[i]);
+        }
     }
 }
